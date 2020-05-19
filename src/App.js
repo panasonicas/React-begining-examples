6e6f3a8954169954a38e8185c7986e6587b0b6ca
panasonicas <58065520+panasonicas@users.noreply.github.com>
2020-05-19 12:38:37 +0300

    2examples

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, {useState} from 'react';
 import Tweet from './Tweet';
 
 function App(){
+  // const [users] = useState([
+  //   {name: 'Ernestas', message:'I am Ernestas', likes:10},
+  //   {name: 'Kamile', message:'I am Kamile, my first letter', likes:11},
+  //   {name: 'Rima', message:'I am Rima, here is my message', likes: 12}
+  // ]);
 
-const [isRed, setRed] = useState(false);
-const [count, setCount] = useState(0);
-const increment = () => {
-  setCount(count+1);
-  setRed(!isRed);
-}  
+  const users = [
+    {name: 'Ernestas', message:'I am Ernestas', likes:10},
+    {name: 'Kamile', message:'I am Kamile, my first letter', likes:11},
+    {name: 'Rima', message:'I am Rima, here is my message', likes: 12}
+  ];  
+
+  
   return(
     <div className="app">
-      <h1 className={isRed ? 'red':'blue'}>change my color</h1>
-      <button onClick={increment}>Increment</button>
-      <h3>{count}</h3>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} likes={user.likes} />
+      ))}
     </div>
   );
 }
