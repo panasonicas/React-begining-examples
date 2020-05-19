38fc6a41ddcc43f1880c2a06227eb35a36508958
panasonicas <58065520+panasonicas@users.noreply.github.com>
2020-05-19 14:01:52 +0300

    reinstalled react router, create nav, abou shop js

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function Nav() {
+  return (
+    <div>
+        <nav>
+            <h3>logo</h3>
+            <ul className="nav-links">
+                <li>About</li>
+                <li>Shop</li>
+            </ul>
+        </nav>
+    </div>
+  );
+}
+
+export default Nav;
