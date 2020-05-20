03d24b56c243ce91092e8d0a155f466a154d2af8
panasonicas <58065520+panasonicas@users.noreply.github.com>
2020-05-20 11:19:55 +0300

    Dinamic routes in shop

--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 function Nav() {
+
+  const naviStyle = {
+  color:'white'
+}
+
   return (
     <div>
         <nav>
             <h3>logo</h3>
             <ul className="nav-links">
+              <Link style={naviStyle} to='/about'>
                 <li>About</li>
+              </Link>
+              <Link style={naviStyle} to='/shop'>
                 <li>Shop</li>
+              </Link>
             </ul>
         </nav>
     </div>
--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
+
 
 function Shop() {
+
+  useEffect(()=>{
+  fechItems();
+},[])
+
+const [items, setItems] = useState([]);
+
+
+
+const fechItems = async () =>{
+  const data = await fetch('https://fortnite-api.theapinetwork.com/store/get');
+  const items = await data.json();
+  console.log(items.data);
+  setItems(items.data);
+};
+
   return (
     <div>
-        <h1>Shop page</h1>
+      <h1>Shop page</h1>
+      {items.map(item =>(
+        <h1 key={item.itemId}>
+          <Link to={`shop/${item.itemId}`}>{item.item.name}</Link>
+        </h1>
+      ))}
     </div>
   );
 }
