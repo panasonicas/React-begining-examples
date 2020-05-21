1ead34b2063b485c9a895958e2d27315058fed89
panasonicas <58065520+panasonicas@users.noreply.github.com>
2020-05-21 17:48:57 +0300

    Applying styles two ways

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const itemStyle = {
+    'backgroundColor':'red',
+    'textTransform' : 'Uppercase'
+};
 
 const TodoItem = ({todo}) => {
     return (
         //inline style markup
-        <div style={{backgroundColor : '#f4f4f4'}}> 
+        //<div style={{backgroundColor : '#f4f4f4'}}> 
+
+        //Style with const itemStyle
+        <div style={itemStyle}> 
             <p>{todo.title}</p>
         </div>
     )
